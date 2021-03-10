a8276a3766ebe589e530d090cf0833083d7dda37
Eddie Jung <ej@sunriseintegration.com>
2021-03-09 17:09:09 -0800

    WTE-49 added theme color styles to TabNavigator.js and StackNavigator.js

--- src/App.js
+++ src/App.js
@@ -2,19 +2,40 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { firebase } from '@react-native-firebase/crashlytics';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
 import DrawerNavigator from './navigation/DrawerNavigator';
 
 const defaultAppCrashlytics = firebase.crashlytics();
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#35b9c0', // main blue
+    accent: '#de6f51', // red orange
+    background: 'white',
+    text: '#1c1e1f',
+    disabled: '#d3d3d3',
+    active: 'white',
+    // placeholder: '',
+    // backdrop: '',
+  },
+  // fonts: {
+
+  // }
+};
 
 /**
  *
  */
 const App = () => {
   return (
-    <NavigationContainer>
-      <DrawerNavigator />
-    </NavigationContainer>
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <DrawerNavigator />
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 
--- src/navigation/StackNavigator.js
+++ src/navigation/StackNavigator.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Button } from 'react-native';
+import { Button } from 'react-native-paper';
 import { createStackNavigator } from '@react-navigation/stack';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { useTheme } from 'react-native-paper';
 
 import Feed from '../screens/Feed';
 import Details from '../screens/Details';
@@ -10,47 +11,63 @@ import SavedRecipes from '../screens/SavedRecipes';
 
 const Stack = createStackNavigator();
 
-const screenOptionStyle = {
-  headerTitleStyle: {
-    fontWeight: 'bold',
-  },
-  headerStyle: {
-    backgroundColor: '#FFF',
-  },
-  // headerTintColor: 'white',
-  // headerBackTitle: 'Back',
+const screenOptionStyle = (colors) => {
+  return {
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+    headerStyle: {
+      backgroundColor: colors.background,
+    },
+    // headerTintColor: 'white',
+    // headerBackTitle: 'Back',
+  };
 };
 
-const optionStyle = {
-  headerTitle: (props) => <Logo {...props} />,
-  headerRight: (props) => (
-    /**
-     * TODO add react native paper and make this an icon
-     */
-    <Button
-      onPress={() => alert('This is a button!')}
-      title="Chrome Cast"
-      color="#35b9c0"
-    />
-  ),
-  headerTitleAlign: 'left',
+const optionStyle = (colors) => {
+  return {
+    headerTitle: (props) => <Logo {...props} />,
+    headerRight: (props) => (
+      /**
+       * TODO add react native paper and make this an icon
+       */
+      <Button
+        onPress={() => alert('This is a button!')}
+        title="Chrome Cast"
+        color="#35b9c0"
+        // mode="outline"
+        icon="cast"
+      />
+    ),
+    headerTitleAlign: 'left',
+  };
 };
 
-const MainStackNavigator = () => (
-  <Stack.Navigator screenOptions={screenOptionStyle}>
-    <Stack.Screen name="Feed" component={Feed} options={optionStyle} />
-    <Stack.Screen name="Details" component={Details} />
-  </Stack.Navigator>
-);
+const MainStackNavigator = () => {
+  const colors = useTheme();
+  return (
+    <Stack.Navigator screenOptions={screenOptionStyle(colors)}>
+      <Stack.Screen
+        name="Feed"
+        component={Feed}
+        options={optionStyle(colors)}
+      />
+      <Stack.Screen name="Details" component={Details} />
+    </Stack.Navigator>
+  );
+};
 
-const SavedRecipeStackNavigator = () => (
-  <Stack.Navigator screenOptions={screenOptionStyle}>
-    <Stack.Screen
-      name="Saved Recipes"
-      component={SavedRecipes}
-      options={optionStyle}
-    />
-  </Stack.Navigator>
-);
+const SavedRecipeStackNavigator = () => {
+  const colors = useTheme();
+  return (
+    <Stack.Navigator screenOptions={screenOptionStyle(colors)}>
+      <Stack.Screen
+        name="Saved Recipes"
+        component={SavedRecipes}
+        options={optionStyle}
+      />
+    </Stack.Navigator>
+  );
+};
 
 export { MainStackNavigator, SavedRecipeStackNavigator };
