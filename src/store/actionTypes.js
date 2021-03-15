fa4fc1ecd54185a1e5d1f77da21324fbe7881e7b
Eddie Jung <ej@sunriseintegration.com>
2021-03-15 11:54:15 -0700

    WTE-19 added fetchAll type and fetchAllRecipes action. Cannot test yet due to not having any api.

--- src/store/actionTypes.js
+++ src/store/actionTypes.js
@@ -1,2 +1,5 @@
+// examples
 export const ADDITION = 'ADDITION';
 export const SUBTRACTION = 'SUBTRACTION';
+
+export const fetchAll = 'recipes/fetchAll';
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,16 +1,30 @@
+// example
 import { ADDITION, SUBTRACTION } from './actionTypes';
 
+import { fetchAll } from './actionTypes';
+
 const initialState = {
+  // example
   counter: 0,
+
+  featured: [],
+  trending: [],
+  holiday: [],
+  popular: [],
+  recent: [],
+  allRecipes: [],
 };
 
-export const mainReducer = (state = initialState, action) => {
+export const rootReducer = (state = initialState, action) => {
   switch (action.type) {
+    // example
     case ADDITION:
       return { ...state, counter: state.counter + 1 };
-
+    // example
     case SUBTRACTION:
       return { ...state, counter: state.counter - 1 };
+    case fetchAll:
+      return { ...state, allRecipes: state.allRecipes };
 
     default:
       return state;
