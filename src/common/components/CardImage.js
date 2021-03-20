63ac90d26c372b61badb548ea4582fa0baff93cf
Eddie Jung <ej@sunriseintegration.com>
2021-03-19 17:00:57 -0700

    WTE-78 Added styling and gradient colors for image. added icon for unavalable images.

--- src/common/components/CardImage.js
+++ src/common/components/CardImage.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const CardImage = (props) => {
   // console.log('cardImage props', JSON.stringify(props, null, 4));
@@ -12,8 +14,22 @@ const CardImage = (props) => {
           style={[styles.container, { width, height }]}
           source={{ uri: item.image }}
         />
-      ) : null}
+      ) : (
+        <View style={{ width, height }}>
+          <View style={styles.imageUnavailable}>
+            <Icon size={100} name="image" color="grey" />
+          </View>
+        </View>
+      )}
       {/* TODO add image not found placeholder text/image here + linear gradient */}
+      <LinearGradient
+        colors={[
+          'rgba(0, 0, 0, 0.1)',
+          'rgba(0, 0, 0, 0.2)',
+          'rgba(0, 0, 0, 0.4)',
+        ]}
+        style={[styles.linearGradient, { width, height }]}
+      />
     </>
   );
 };
@@ -22,6 +38,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     borderRadius: 10,
+  },
+  linearGradient: {
+    position: 'absolute',
+    borderRadius: 10,
+  },
+  imageUnavailable: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
