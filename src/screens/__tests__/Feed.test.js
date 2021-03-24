4079df554a823718bf680923efef9ca7f509da25
Eddie Jung <ejung716@gmail.com>
2021-03-24 11:13:05 -0700

    WTE-100 Create a mock settings file. Pass the first test.

--- src/screens/__tests__/Feed.test.js
+++ src/screens/__tests__/Feed.test.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
+import { Provider, useDispatch } from 'react-redux';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 
-import Feed from '../index';
+import { Feed } from '../index';
+import rootReducer from '../../app/rootReducer';
 
-jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
+const ReduxProvider = ({ children, reduxStore }) => (
+  <Provider store={reduxStore}>{children}</Provider>
+);
 
 it('render default text', () => {
-  const screen = render(<Feed />);
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: [...getDefaultMiddleware({ immutableCheck: false })],
+  });
+  const wrapper = ({ children }) => (
+    <ReduxProvider reduxStore={store}>{children}</ReduxProvider>
+  );
+  // const dispatch = useDispatch();
+  const screen = render(
+    <Provider store={store}>
+      <Feed />
+    </Provider>,
+  );
 
   screen.getByText('Popular');
 });
