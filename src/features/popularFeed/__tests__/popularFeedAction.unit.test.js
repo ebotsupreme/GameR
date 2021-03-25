29200100026b199331e394caf08a85559ee6fff5
Eddie Jung <ejung716@gmail.com>
2021-03-25 16:24:42 -0700

    WTE-100 Rename recent to popular for feed and references. Cleanup code.

--- src/features/popularFeed/__tests__/popularFeedAction.unit.test.js
+++ src/features/popularFeed/__tests__/popularFeedAction.unit.test.js
@@ -1,7 +1,3 @@
-/**
- * TODO: Not sure if I need to use thunk or toolkit's createAsyncThunk.
- * I do need to test the different calls to the reducer from the slice.
- */
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import Config from 'react-native-config';
