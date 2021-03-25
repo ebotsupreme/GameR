967658c8c7c628f0f315ecfeb85ebdddc2bd77e8
Eddie Jung <ejung716@gmail.com>
2021-03-24 17:34:36 -0700

    WTE-100 Install redux-mock-store. Create an empty popularFeedAction unit test file with a todo.

---
+++ src/features/recentFeed/__tests__/popularFeedAction.unit.test.js
@@ -0,0 +1,5 @@
+/**
+ * TODO: Not sure if I need to use thunk or toolkit's createAsyncThunk.
+ * I do need to test the different calls to the reducer from the slice.
+ */
+import configureMockStore from 'redux-mock-store';
