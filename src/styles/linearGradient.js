576f630d1a3b919be62bc397ef59cc1ca0dbefdf
Eddie Jung <ejung716@gmail.com>
2021-04-08 13:24:45 -0700

    WTE-107 initial commit. Update gradient colors.

--- src/styles/linearGradient.js
+++ src/styles/linearGradient.js
@@ -1,7 +1,7 @@
 const feedGradientColors = [
   'rgba(0, 0, 0, 0.0)',
   'rgba(0, 0, 0, 0.1)',
-  'rgba(0, 0, 0, 0.1)',
+  'rgba(0, 0, 0, 0.2)',
   'rgba(0, 0, 0, 0.2)',
   'rgba(0, 0, 0, 0.3)',
   'rgba(0, 0, 0, 0.4)',
